f1db82f4313ee4386877f5904d8a079c842d3d17
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2022-09-17 14:42:36 +0300

    commit

--- app.js
+++ app.js
@@ -43,31 +43,83 @@ inputDom.addEventListener("keydown", (event) => {
   }
 });
 
-//? del Event Handler
+//? check and del Event Handler
 
 listDom.addEventListener("click", (event) => {
   event.preventDefault();
   const item = event.target;
-  if (item.classList[0] == "fa-solid") {
+  if (item.classList == "fa-solid fa-trash") {
     const delParent = item.parentElement;
     delParent.classList.add("delete");
     delParent.addEventListener("transitionend", () => {
       delParent.remove();
     });
   }
-  if (item.classList[0] === "fa-regular") {
+  if (item.classList == "fa-regular fa-circle") {
     //  <i class="fa-regular fa-circle"></i>;
     //  <i class="fa-regular fa-circle-check"></i>
-    const checkIn = document.getElementsByClassName("fa-regular");
-    const checkParent = item.parentElement;
-    checkParent.classList.toggle("complete");
-    console.log(checkIn[0]);
-    // if (checkIn.classList) {
-    //   checkIn.classList.remove("fa-circle");
-    //   checkIn.classList.add("fa-circle-check");
-    // } else {
-    //   checkIn.classList.add("fa-circle");
-    //   checkIn.classList.remove("fa-circle-check");
-    // }
+    item.classList.remove("fa-circle");
+    item.classList.add("fa-circle-check");
+    item.parentElement.classList.add("complete");
+    console.log("check on");
+    console.log(item.classList);
+  } else if (item.classList == "fa-regular fa-circle-check") {
+    item.classList.remove("fa-circle-check");
+    item.classList.add("fa-circle");
+    item.parentElement.classList.remove("complete");
+    console.log("check out");
+    console.log(item.classList);
   }
+});
+
+//? clock and date
+const day = document.querySelector("#day");
+const month = document.querySelector("#month");
+const year = document.querySelector("#year");
+const dayWord = document.querySelector("#dayWord");
+
+const days = [
+  "Pazar",
+  "Pazartesi",
+  "Salı",
+  "Çarşamba",
+  "Perşembe",
+  "Cuma",
+  "Cumartesi",
+];
+
+const d = new Date().getDay();
+const m = new Date().getMonth();
+const y = new Date().getFullYear();
+
+day.innerText = d;
+month.innerText = m;
+year.innerText = y;
+dayWord.innerText = days[d];
+
+//? Fİlter
+
+filterDom.addEventListener("click", (e) => {
+  const todo = listDom.childNodes;
+  todo.forEach((item) => {
+    switch (e.target.value) {
+      case "all":
+        item.style.display = "flex";
+        break;
+      case "completed":
+        if (item.classList.contains("complete")) {
+          item.style.display = "flex";
+        } else {
+          item.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!item.classList.contains("complete")) {
+          item.style.display = "flex";
+        } else {
+          item.style.display = "none";
+        }
+        break;
+    }
+  });
 });
